26b0bf5f7435f3c0621871ca9a19cea4a7adb4e4
Sonic21354 <85310527+Sonic21312213@users.noreply.github.com>
2024-10-07 19:46:27 +0200

    t

--- js/interest.js
+++ js/interest.js
@@ -1,14 +1,29 @@
 console.log("First mes.");
-
-
 console.log("Second mes.");
 
-const testbut = document.querySelector('.testbut')
-
+const testbut = document.querySelector('.testbut');
+const body = document.body;
 
 testbut.addEventListener('click', () => {
+    // Add the flashing effect to the button
+    testbut.classList.add('flash');
+    
+    // Add the flashing background effect
+    body.classList.add('background-flash');
+
+    // Go fullscreen
+    if (document.documentElement.requestFullscreen) {
+        document.documentElement.requestFullscreen();
+    }
+
     setTimeout(() => {
         const audio = new Audio('./sounds/test.mp3');
         audio.play();
-    }, 500)
+
+        // Stop the flashing effect after a few seconds
+        setTimeout(() => {
+            testbut.classList.remove('flash');
+            body.classList.remove('background-flash'); // Remove background flash
+        }, 2000); // Flashing lasts for 2 seconds
+    }, 500);
 });
